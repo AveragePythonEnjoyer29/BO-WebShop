e656670c100a0556275a5c2610ed5f36b5adda27
AveragePythonEnjoyer29 <115230428+AveragePythonEnjoyer29@users.noreply.github.com>
2023-06-14 13:57:21 +0200

    grote push
    bezig met converten van fotos naar webp, en met reviews

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Based.game
 - Onderwerp: computers, laptops en laptop accessoires
 - Style: https://www.gamemania.nl/nl
+- Wireframe: https://miro.com/app/board/uXjVMRFp2ko=/
--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,55 @@
-let shoppingcart = {
-    "price": 0.0,
-    "items": {}
+let products = [];
+let reviews = [];
+
+/* Displays the products in a miniature field */
+function display_product_mini(element, data) {
+
+    element.innerHTML = `
+    <div class="center">
+        <img src="${data.img}" alt="product afbeelding" class="product--img">
+        <p class="product--name">${data.name}</p>
+        <div>
+            <p class="product--price">${data.price},-</p>
+            ${data.in_stock ? "" : `<p class="product--no-stock">Out of stock!</p>`}
+            ${data.is_ad ? `<p class="product--advertisement">Advertisement</p>` : ""}
+        </div>
+    </div>`;
 }
 
-function add_shoppingcart(name, item) {
-
-    // add new item
-    shoppingcart["items"][name] = item;
-
-    // update price
-    shoppingcart["price"] += item["price"];
+function display_review(element, data) {
+    element.innerHTML = `
+    <h2 id="review--text">${data.text}</h2>
+    <p id="review--stars">${data.stars}</p>
+    <p id="review--name">${data.name}</p>
+    `
 }
 
-function del_shoppingcart(name) {
-
-    if (name in shoppingcart.items) {
-        // update price
-        shoppingcart["price"] -= shoppingcart["items"][name]["price"];
-
-        // remove item
-        delete shoppingcart[name];
+// Fetch products from file
+fetch("/static/data/products.json")
+.then(function (response) { return response.json(); })
+.then(function(json) {
+    for (let i = 0; i < json.length; i++) {
+        products[i] = json[i];
     }
-}
+
+    for (let i = 0; i < 4; i++) {
+        const element = document.getElementById("card" + (i + 1));
+        display_product_mini(element, products[i]);
+    }
+});
+
+// Fetch reviews from file
+/*
+fetch("/static/data/reviews.json")
+.then(function (response) { return response.json(); })
+.then(function(json) {
+    for (let i = 0; i < json.length; i++) {
+        reviews[i] = json[i];
+    }
+
+    for (let i = 0; i < 1; i++) {
+        const element = document.getElementById("review" + (i + 1));
+        display_review(element, reviews[i]);
+    }
+});
+*/
